e139f93d77a878cc41e1654ed55428524d76d17a
DennisPieper <dennispieperr@gmail.com>
2024-09-24 21:12:37 +0200

    Push

--- app.py
+++ app.py
@@ -1,51 +1,49 @@
-import io
+# import io
 from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from transaktion import transaktion
-from KontostandAnzeigen import getKonto
-from createDiagramm import createDia
+# from createDiagramm import createDia
+
+import psycopg2
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/data', methods=['POST'])
-def post_data():
-
-    data = request.json  
-    
-    betrag = data.get('balance')
-    status = data.get('status')
-    
-    if status == "Einzahlung":
-        tran = transaktion(eingang=betrag, verwendungszweck="Geld wurde eingezahlt")
-        tran.doTransaktion()
-    else:
-        tran = transaktion(abgang=betrag, verwendungszweck="Geld wurde ausgezahlt")
-        tran.doTransaktion()
-        
-    
-    return jsonify({'received': data})
-
-#Brauchst du eig nicht
-# @app.route('/getKontostand')
-# def getKontostand():
-#     konto = getKonto()
-#     result = konto.SQLGetKonto()
-#     return jsonify({'kontostand': result})
+def connectDB():
+    connection = psycopg2.connect(
+            host='aws-0-eu-central-1.pooler.supabase.com',        
+            user='postgres.jpvtmjvrpafzjvoghojs',  
+            password='Dsde22.11.23FD',              
+            database='postgres',
+            port=6543  
+        )
+    print("Verbindung erstellt")
+    cursor = connection.cursor()
+    return cursor, connection
 
 @app.route('/getPerson', methods=['GET'])
 def getPerson():
+    cursor, connection = connectDB()
+    cursor.execute("""SELECT * FROM Users""")
+    
+    userData = cursor.fetchall()
+
+    cursor.execute("""SELECT "Kontostand" FROM Einauszahlungen where "UserID" = {} order by "TransaktionsID" limit 1""".format(userData[0][0]))
+    balance = cursor.fetchall()
+
+    cursor.execute("""SELECT "Verwendung" FROM Verwendungen where "UserID" = {}""".format(userData[0][0]))
+    verwendungsZweckList = cursor.fetchall()
+
     #Bsp:
     # id = 1
     # name = "Max Mustermann"
     # balance = 1000
     # verwendungsZweckList = ['Einzahlung', 'Auszahlung']
 
-    #SQL Abfrage, die User Daten aus der Datenbank holt
-
-    #return jsonify({'id': id, 'name': name, 'balance': balance, 'list': verwendungsZweckList})
-    pass
+    connection.commit()
+    return jsonify({'id': userData[0][0], 'name': userData[0][6], 'balance': balance[0][0], 'list': verwendungsZweckList[0]})
+    
 
 @app.route('/putPerson', methods=['POST'])
 def putPerson():
@@ -56,17 +54,14 @@ def putPerson():
 def putBuchung():
     #SQL Abfrage, die Buchung in der Datenbank updated
     #Daten: ID, Betrag, Verwendungszweck, Status(Einzahlung/Auszahlung)
-    pass
+    data = request.get_json()
+    cursor, connection = connectDB()
+    UserID = data.get('id')
+    status = data.get('Status')
+    betrag = data.get('betrag')
+    verwendungsid = data.get('Verwendungszweck')
+    newTransaktion = transaktion(cursor=cursor, connection=connection)
     
-
-@app.route('/getImage')
-def createDiagramm():
-    diag = createDia()
-    buf = io.BytesIO()
-    diag.createDia().savefig(buf, format="png")
-    buf.seek(0)
-    
-    return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
