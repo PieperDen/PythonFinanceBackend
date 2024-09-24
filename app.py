ade6cc09508b0be1e18c1f8423501b9d8ca77634
Mika <mika.koenigsmann@stdt.dhsh.de>
2024-09-24 20:19:26 +0200

    Get Abfrage fertig

--- app.py
+++ app.py
@@ -22,49 +22,28 @@ def connectDB():
     cursor = connection.cursor()
     return cursor, connection
 
-# @app.route('/api/data', methods=['POST'])
-# def post_data():
-
-#     data = request.json  
-    
-#     betrag = data.get('Betrag')
-#     verwendung = data.get('verwendung')
-    
-#     if verwendung == "Einzahlung":
-#         tran = transaktion(eingang=betrag, verwendungszweck="Geld wurde eingezahlt")
-#         tran.doTransaktion()
-#     else:
-#         tran = transaktion(abgang=betrag, verwendungszweck="Geld wurde ausgezahlt")
-#         tran.doTransaktion()
-        
-    
-#     return jsonify({'received': data})
-
-#Brauchst du eig nicht
-# @app.route('/getKontostand')
-# def getKontostand():
-#     konto = getKonto()
-#     result = konto.SQLGetKonto()
-#     return jsonify({'kontostand': result})
-
 @app.route('/getPerson', methods=['GET'])
 def getPerson():
     cursor, connection = connectDB()
     cursor.execute("""SELECT * FROM Users""")
     
     userData = cursor.fetchall()
-    
+
+    cursor.execute("""SELECT "Kontostand" FROM Einauszahlungen where "UserID" = {} order by "TransaktionsID" limit 1""".format(userData[0][0]))
+    balance = cursor.fetchall()
+
+    cursor.execute("""SELECT "Verwendung" FROM Verwendungen where "UserID" = {}""".format(userData[0][0]))
+    verwendungsZweckList = cursor.fetchall()
+
     #Bsp:
     # id = 1
     # name = "Max Mustermann"
     # balance = 1000
     # verwendungsZweckList = ['Einzahlung', 'Auszahlung']
 
-    #SQL Abfrage, die User Daten aus der Datenbank holt
-
-    #return jsonify({'id': userData[0][0], 'name': user[0][6], 'balance': balance, 'list': verwendungsZweckList})
     connection.commit()
-    return jsonify({'received': cursor.fetchall()})
+    return jsonify({'id': userData[0][0], 'name': userData[0][6], 'balance': balance[0][0], 'list': verwendungsZweckList[0]})
+    
 
 @app.route('/putPerson', methods=['POST'])
 def putPerson():
@@ -76,16 +55,6 @@ def putBuchung():
     #SQL Abfrage, die Buchung in der Datenbank updated
     #Daten: ID, Betrag, Verwendungszweck, Status(Einzahlung/Auszahlung)
     pass
-    
-
-# @app.route('/getImage')
-# def createDiagramm():
-#     diag = createDia()
-#     buf = io.BytesIO()
-#     diag.createDia().savefig(buf, format="png")
-#     buf.seek(0)
-    
-#     return send_file(buf, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
